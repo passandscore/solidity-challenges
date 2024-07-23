5939bc54788f0b57904f3a8e62787e3ccbd78c08
Akwuba Christopher <uba69@Akwubas-MacBook-Pro.local>
2024-07-23 13:23:41 +0100

    Challenge 6

---
+++ Challenges/challenge-06/README.md
@@ -0,0 +1,9 @@
+# Challenge 06
+
+## GOAL
+
+The goal of this challenge is simple. You are to drain this contract.
+
+
+NOTE: You are expected to deploy the contract with any amount greater than the `Entrance Fee`.
+ 
---
+++ Challenges/challenge-06/challenge.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+contract Registration {
+    using Address for address payable;
+    
+    enum Status {
+        Unregistered,
+        Pending,
+        Approved
+    }
+
+    address public host = 0xdDE7B987A01717EEfCcA1Dc5280c164E2cCD133e;
+    uint256 public ENTRANCE_FEE = 0.01 ether;
+    mapping(address players => Status) public playersStatus;
+
+    constructor() payable {
+        require(msg.value > ENTRANCE_FEE);
+    }
+
+    function register() public payable {
+        require(msg.value == ENTRANCE_FEE, "Incorrect Entrance Fees");
+        require(playersStatus[msg.sender] != Status.Pending, "Initial Registration Pending");
+        playersStatus[msg.sender] = Status.Pending;
+    }
+
+    function approvePlayer(address player) public {
+        require(msg.sender == host, "Not Approved");
+        require(playersStatus[msg.sender] == Status.Pending, "Yet to Register");
+        playersStatus[player] = Status.Approved;
+    }
+
+    function withdrawAllFees() external {
+        require(msg.sender == host, "Not Authorized");
+        uint256 fees = address(this).balance;
+        (bool success, ) = msg.sender.call{value: fees}("");
+        require(success, "Failed to withdraw");
+    }
+
+    receive() external payable {
+        require(msg.value > (ENTRANCE_FEE * 5));
+        host = msg.sender;
+    }
+}
+
+
