8777658b31bee0f777ff4b1737629afbc2623519
Jason Schwarz <jason.c.schwarz@gmail.com>
2024-08-11 08:01:33 -0300

    ✂️ refactor - update solidity challenge #1 test

--- test/solidity/challenge-01/Challenge01.t.sol
+++ test/solidity/challenge-01/Challenge01.t.sol
@@ -5,34 +5,31 @@ import "../../../src/solidity/challenge-01/Challenge01.sol";
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract Solution is Test {
     Challenge01 internal contractUnderTest;
 
-    address payable deployer = payable(makeAddr("deployer"));
     address payable unauthorizedUser = payable(makeAddr("unauthorizedUser"));
 
     function setUp() public {
         string memory name = "MockERC721";
         string memory symbol = "MOCK";
 
-        contractUnderTest = new Challenge01(
-            name,
-            symbol
-        );
+        contractUnderTest = new Challenge01(name, symbol);
 
         // label the contract
         vm.label(address(contractUnderTest), name);
 
         // label the EOAs
-        vm.label(deployer, "deployer");
         vm.label(unauthorizedUser, "unauthorizedUser");
     }
-
 
     function test_should_revert_when_attempting_to_mint_as_unauthorized_user()
         external
     {
-        revert(); // Provide solution here
+        vm.startPrank(unauthorizedUser);
+
+        /**
+         * Provide solution here
+         */
     }
 }
